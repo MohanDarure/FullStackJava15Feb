92e36996ff729569611c3eb0023ec6791de981db
MohanDarure <mohandarure436@gmail.com>
2021-02-26 15:39:21 -0500

    second commite

---
+++ src/sample/Test1.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class Test1 {
+    public void name() {
+        System.out.println("mohan");
+
+    }
+
+   public void rollno() {
+        System.out.println(20);
+    }
+
+    public static void main(String[] args) {
+        Test1 sample = new Test1();
+        sample.name();
+        sample.rollno();
+
+    }
+}
